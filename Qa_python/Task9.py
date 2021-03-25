7dc42a004afbb8f5855ab9afa9454526a4a5b81e
kozlyakovskiyma <maxim.kazliakouski@gmail.com>
2021-03-25 15:32:58 +0300

    Adding exercise to Task9.py

--- Qa_python/Task9.py
+++ Qa_python/Task9.py
@@ -1,19 +1,35 @@
-# ---- Task 3 ----
-price = {'menu_1': 12.00, 'menu_2': 13.40}
+# # ---- Task 3 ----
+# price = {'menu_1': 12.00, 'menu_2': 13.40}
+#
+#
+# class Lunch:
+#     def __init__(self, menu):
+#         self.menu = str(menu)
+#
+#     def menu_price(self, choice=int(input('Make your menu choice: '))):
+#         if choice == 1:
+#             print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_1']))
+#         elif choice == 2:
+#             print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_2']))
+#         else:
+#             print('Error menu')
+#
+#
+# Paul = Lunch('Menu 1')
+# Paul.menu_price()
+
+# ---- Task 4 ----
 
 
-class Lunch:
-    def __init__(self, menu):
-        self.menu = str(menu)
+class Point3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-    def menu_price(self, choice=int(input('Make your menu choice: '))):
-        if choice == 1:
-            print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_1']))
-        elif choice == 2:
-            print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_2']))
-        else:
-            print('Error menu')
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.x, self.y, self.z)
 
 
-Paul = Lunch('Menu 1')
-Paul.menu_price()
+my_point = Point3D(1, 2, 3)
+print(my_point.__repr__())
