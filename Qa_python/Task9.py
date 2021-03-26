c1ec0304e0a0d20235f1435e70e799311a2f872c
kozlyakovskiyma <maxim.kazliakouski@gmail.com>
2021-03-26 09:41:52 +0300

    adding changis from work place

--- Qa_python/Task9.py
+++ Qa_python/Task9.py
@@ -1,22 +1,22 @@
-# # ---- Task 3 ----
-# price = {'menu_1': 12.00, 'menu_2': 13.40}
-#
-#
-# class Lunch:
-#     def __init__(self, menu):
-#         self.menu = str(menu)
-#
-#     def menu_price(self, choice=int(input('Make your menu choice: '))):
-#         if choice == 1:
-#             print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_1']))
-#         elif choice == 2:
-#             print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_2']))
-#         else:
-#             print('Error menu')
-#
-#
-# Paul = Lunch('Menu 1')
-# Paul.menu_price()
+# ---- Task 3 ----
+price = {'menu_1': 12.00, 'menu_2': 13.40}
+
+
+class Lunch:
+    def __init__(self, menu):
+        self.menu = str(menu)
+
+    def menu_price(self, choice=int(input('Make your menu choice: '))):
+        if choice == 1:
+            print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_1']))
+        elif choice == 2:
+            print(f'Your choice is {choice}', end='\nThe price is: {}$'.format(price['menu_2']))
+        else:
+            print('Error menu')
+
+
+Paul = Lunch('Menu 1')
+Paul.menu_price()
 
 # ---- Task 4 ----
 
@@ -33,3 +33,12 @@ class Point3D:
 
 my_point = Point3D(1, 2, 3)
 print(my_point.__repr__())
+
+from Task8 import ATM
+
+user1 = ATM()
+if user1.user_initialization():
+    user1.verifying_pin_code(input('Pin-code: '))
+else:
+    print('Access denied!')
+
