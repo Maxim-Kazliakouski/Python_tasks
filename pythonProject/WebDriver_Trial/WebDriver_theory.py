53a83a0db9f6eef62186d528d9d132470b8029a1
Maxim-Kazliakouski <maxim.kazliakouski@gmail.com>
2021-03-30 00:20:28 +0300

    29.03.21 work at home

--- pythonProject/WebDriver_Trial/WebDriver_theory.py
+++ pythonProject/WebDriver_Trial/WebDriver_theory.py
@@ -3,10 +3,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-# # установка Driver Manager для Chrome
-# driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+# установка Driver Manager для Chrome
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 # после выполнения какого-либо действия драйвер ждёт 5 секунд появления интересующего нас элемента
 driver.implicitly_wait(5)
 # наш искомый URL, который будет отображаться при открытии браузера
